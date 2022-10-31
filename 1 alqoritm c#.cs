b515ac3f6b34440fd981fc034258fa93f2cf1c60
Ilqar03 <tu757r7k9@code.edu.az>
2022-10-31 11:31:41 +0400

    alqoritm

---
+++ 1 alqoritm c#.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            int N=Convert.ToInt32(Console.ReadLine());
+
+            for(int i=1; i<=N; i++)
+            {
+                count *= i;
+            }
+            Console.WriteLine(N+"faktoriali"+count);
+        }
+    }
+}
---
+++ 2 ci alqoritm c#.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+                int N=Convert.ToInt32(Console.ReadLine());
+            if (N==1)
+            {
+                Console.WriteLine("1 ne musbet nede menfi ededdir");
+            }
+            for (int i = 1; i <= N; i++)
+            {
+                if (N % i == 0)
+                {
+                    count += 1;
+                }
+            }
+            if (count == 2)
+                {
+                    Console.WriteLine("eded sade ededdir");
+                }
+                else
+                {
+                    Console.WriteLine("eded murekkeb ededdir");
+                }
+            
+
+        }
+    }
+}using System;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+                int N=Convert.ToInt32(Console.ReadLine());
+            if (N==1)
+            {
+                Console.WriteLine("1 ne musbet nede menfi ededdir");
+            }
+            for (int i = 1; i <= N; i++)
+            {
+                if (N % i == 0)
+                {
+                    count += 1;
+                }
+            }
+            if (count == 2)
+                {
+                    Console.WriteLine("eded sade ededdir");
+                }
+                else
+                {
+                    Console.WriteLine("eded murekkeb ededdir");
+                }
+            
+
+        }
+    }
+}
